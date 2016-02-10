abdcadf64827fa956191ccb9d2ea0b889b7948e1
Erik Anderson <Anderson.erik.n@gmail.com>
2016-02-10 07:59:25 +0000

    finalized code

--- avatar.js
+++ avatar.js
@@ -2,41 +2,48 @@ angular.module('app', [])
   .controller('mainCtrl', mainCtrl)
   .directive('avatar', avatarDirective);
 
-function mainCtrl ($scope) {
+function mainCtrl ($scope) 
+{
 
   $scope.users = [];
 
-  $scope.addNew = function (user) {
-    $scope.users.push({ 
+  $scope.addNew = function (user) 
+{
+    $scope.users.push(
+{ 
       name: user.name,
       avatarUrl: user.url
-    }); /* [1] */
+});
     
-    user.name = ''; /* [2] */
+    user.name = '';
     user.url = '';
   };
 }
 
-function avatarDirective () {
-  return {
-    scope: {
-      user: '=' /* [1] */
-    },
-    restrict: 'E', /* [2] */
+function avatarDirective () 
+{
+  return 
+{
+    scope: 
+{
+      user: '='
+},
+    restrict: 'E',
     template: (
       '<div class="Avatar">' +
         '<img ng-src="{{user.avatarUrl}}" />' +
         '<h4>{{user.name}}</h4>' +
       '</div>'
-    ), /* [3] */
+    ),
     link: link
-  };
+};
   
-  function link (scope) { /* [4] */
-    if (!scope.user.avatarUrl) {
+  function link (scope) 
+{
+    if (!scope.user.avatarUrl) 
+{
       scope.user.avatarUrl = 'http://thealmanac.org/assets/img/default_avatar.png';
-    }
-  }
-
+}
 }
 
+}
